734e72b2bcc18b7661d7fe3f7cd24ae1e6b99c9d
Bladimir Lopez Amendras <bladimir.lopez.almendras@gmail.com>
2021-06-14 12:57:40 -0400

    terminado el proyecto ya funcionando con backend

--- src/App.js
+++ src/App.js
@@ -1,53 +1,91 @@
-import React,{Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
-// data
-import { todos } from './todos.json';
-// subcomponents
-import TodoForm from './components/TodoForm';
-console.log(todos);
-//import Navigation from './components/Navigation';
+import React, { Component } from 'react'; 
+import logo from './logo.svg'; 
+import './App.css'; 
+// data 
+//import { todos } from './todos.json'; 
+// subcomponents 
+import TodoForm from './components/TodoForm'; 
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      todos
-    }
-    this.handleAddTodo = this.handleAddTodo.bind(this);
+import request from 'superagent';
+
+const superagentid = require('superagent');
+
+/////console.log(todos); 
+
+
+
+
+
+class App extends Component { 
+
+  constructor() { 
+    super(); 
+    this.state = { 
+    todos: [] 
+    
+  } 
+  this.handleAddTodo = this.handleAddTodo.bind(this); 
   }
-  removeTodo(index) {
-    this.setState({
-      todos: this.state.todos.filter((e, i) => {
-        return i !== index
+
+  componentDidMount(){
+    request
+      .get('http://192.168.0.17:8000/deposito')
+      .end((err,res)=>{
+        // console.log(res);  
+         console.log(JSON.parse(res.text));
+         const deposito =JSON.parse(res.text);
+         console.log(deposito);
+         this.setState({
+           todos: deposito
+         });
+      });
+  }
+
+  removeTodo(index,posiId) { 
+    const elimIndex=index+1;
+    console.log("entra eliminar");
+    console.log(elimIndex);
+    //superagentd.delete("http://192.168.0.101:8000/zoologico/8").end((err,res)=>{});
+    //superagentd.delete("http://192.168.0.101:8000/zoologico/"+elimIndex).end((err,res)=>{});
+    superagentid.delete("http://192.168.0.17:8000/deposito/"+posiId).end((err,res)=>{});
+    this.setState({ 
+      todos: this.state.todos.filter((e, i) => { 
+        return i !== index 
       })
-    });
-  }
-  handleAddTodo(todo) {
-    this.setState({
-      todos: [...this.state.todos, todo]
+    }); 
+  } 
+
+  handleAddTodo(todo) { 
+    this.setState({ 
+      todos: [...this.state.todos, todo] 
     })
   }
+
+
   render() {
     const todos = this.state.todos.map((todo, i) => {
       return (
         <div className="col-md-4" key={i}>
           <div className="card mt-4">
+
             <div className="card-title text-center">
               <h3>{todo.id}</h3>
               <span className="badge badge-pill badgedanger ml-2">
                 {todo.codigo}
               </span>
             </div>
+
             <div className="card-body">
               {todo.descripcion}
             </div>
+
             <div className="card-footer">
               <button className="btn btn-danger"
-                onClick={this.removeTodo.bind(this, i)}>
+                onClick={this.removeTodo.bind(this, i, todo.id)}>
                 Eliminar
               </button>
             </div>
+
           </div>
         </div>
       )
@@ -67,8 +105,7 @@ class App extends Component {
             <div className="row mt-4">
               <div className="col-md-4 text-center">
                 <img src={logo} className="App-logo" alt="logo" />
-                <TodoForm 
-                  onAddTodo={this.handleAddTodo}>
+                <TodoForm onAddTodo={this.handleAddTodo}>
                 </TodoForm>
               </div>
               <div className="col-md-8">
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,73 +1,111 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'; 
+//import request from 'superagent';
 
-class TodoForm extends Component {
-    constructor () {
-        super();
-        this.state = {
-            id: '',
-            codigo: '',
+const superagent = require('superagent');
+
+class TodoForm extends Component { 
+
+  constructor () { 
+    super(); 
+        this.state = { 
+            id: '', 
+            codigo: '', 
             descripcion: ''
-        };
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        }; 
+
+        this.handleInputChange = this.handleInputChange.bind(this); 
+        this.handleSubmit = this.handleSubmit.bind(this); 
     }
-    handleSubmit(e) {
-        e.preventDefault();
-        this.props.onAddTodo(this.state);
-        this.setState({
-            id: '',
-            codigo: '',
-            descripcion: ''
-        });
-    }
-    handleInputChange(e) {
-        const {value, name} = e.target;
-        console.log(value, name);
-        this.setState({
-            [name]: value
-        });
-    }
-    render() {
-        return (
-            <div className="card">
-                <form onSubmit={this.handleSubmit} className="card-body">
-                    <div className="form-group">
-                        <input
-                            type="text"
-                            name="id"
-                            className="form-control"
-                            value={this.state.title}
-                            onChange={this.handleInputChange}
-                            placeholder="Id"
-                        />
-                    </div>
-                    <div className="form-group">
-                        <input
-                            type="text"
-                            name="codigo"
-                            className="form-control"
-                            value={this.state.responsible}
-                            onChange={this.handleInputChange}
-                            placeholder="Codigo"
-                        />
-                    </div>
-                    <div className="form-group">
-                        <input
-                            type="text"
-                            name="descripcion"
-                            className="form-control"
-                            value={this.state.description}
-                            onChange={this.handleInputChange}
-                            placeholder="Descripcion"
-                        />
-                    </div>
  
-                    <button type="submit" className="btn btnprimary">
-                        guardar
-                    </button>
-                </form>
-            </div>
-        )
+    handleSubmit(e) { 
+    
+        e.preventDefault(); 
+        this.props.onAddTodo(this.state); 
+
+
+        this.setState({ 
+            id: '', 
+            codigo: '', 
+            descripcion: ''
+        }); 
+
+        console.log("Enviar Dato");
+
+        console.log(this.state.id," ",this.state.codigo,);
+        const a=this.state.id;
+        const b=this.state.codigo;
+        //const c=this.state.pais;
+        const d=this.state.descripcion;
+
+
+        superagent.post('http://192.168.0.17:8000/deposito')
+        .send({ id:a.toString(),codigo:b.toString(),descripcion:d.toString()})
+        .end((err,res)=>{});
+
+
+        //este codigo si manda
+        //superagent.post('http://192.168.0.100:8000/zoologico')
+        //       .send({ nombre:"sacaba",ciudad:"colomi",pais:"0",tamanio:4,PresupuestoAnual:2})
+        //       .end((err,res)=>{});
+        ///
+
     }
+
+    handleInputChange(e) { 
+        const {value, name} = e.target; 
+        console.log(value, name); 
+        this.setState({ 
+            [name]: value 
+        }); 
+    } 
+
+
+    render() { 
+        return ( 
+            <div className="card"> 
+                <form onSubmit={this.handleSubmit} className="card-body"> 
+    
+                    <div className="form-group"> 
+                        <input 
+                            type="text" 
+                            name="id" 
+                            className="form-control" 
+                            value={this.state.responsible} 
+                            onChange={this.handleInputChange} 
+                            placeholder="id" 
+                        /> 
+                    </div> 
+
+
+                    <div className="form-group"> 
+                        <input 
+                            type="text" 
+                            name="codigo" 
+                            className="form-control" 
+                            value={this.state.description} 
+                            onChange={this.handleInputChange} 
+                            placeholder="codigo" 
+                        /> 
+                    </div> 
+
+                    <div className="form-group"> 
+                        <input 
+                            type="text" 
+                            name="descripcion" 
+                            className="form-control" 
+                            value={this.state.description} 
+                            onChange={this.handleInputChange} 
+                            placeholder="descripcion" 
+                        /> 
+                    </div> 
+
+    
+                    <button type="submit" className="btn btn-primary"> 
+                        Guardar 
+                    </button> 
+                </form> 
+            </div> 
+        ) 
+    } 
 }
-export default TodoForm;
+    export default TodoForm;
